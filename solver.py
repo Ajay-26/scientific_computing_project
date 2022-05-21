8b110fa4e457547a7d5cfae2d79a5719b9b4b2d5
Ajay-26 <aj.sailopal@gmail.com>
2022-05-20 23:23:48 -0500

    Initial Done

--- solver.py
+++ solver.py
@@ -1,9 +1,17 @@
 from utils import *
 
-def euler_shift(vorticity, psi, velocities,dt):
-	return vorticity.copy()
+def euler_shift(vorticity, psi, velocities,dt,dx,diffusivity):
+	#Using simple euler timestep shift, 
+	#(w_new[i,j] - w[i,j])/dt + u*(w[i+1,j] - w[i-1,j])/2*dx + v*(w[i,j+1] - w[i,j-1])/2*dy = diffusivity*((w[i+1,j] + w[i-1,j] - 2*w[i,j])/dx*dx + (w[i,j+1] + w[i,j-1] - 2*w[i,j])/dy*dy)
+	new_vorticity = vorticity.copy()
+	m,n = vorticity.shape[0],vorticity.shape[1]
+	#Try to reformulate using numpy
+	for i in range(m):
+		for j in range(n):
+			new_vorticity[i,j] = diffusivity*((w[i+1,j] + w[i-1,j] - 2*w[i,j])/(dx*dx) + (w[i,j+1] + w[i,j-1] - 2*w[i,j])/(dx*dx)) - u*(w[i+1,j] - w[i-1,j])/2*dx - v*(w[i,j+1] - w[i,j-1])/(2*dx)
+	return new_vorticity
 
-def vorticity_solver(vorticity,velocity,diffusivity,vel_func,dt,ntime_steps,L=1):
+def vorticity_solver(vorticity,velocity,diffusivity,vel_func,dt,dx,ntime_steps,L=1):
 	#Vorticity is a matrix of problem dimension size
 	#velocity can be a function of the coordinates, or even constant
 	#diffusivity is the coefficient of "damping"
@@ -23,10 +31,10 @@ def vorticity_solver(vorticity,velocity,diffusivity,vel_func,dt,ntime_steps,L=1)
 		vort_f = np.fft.fft(vorticity)
 		kappa = np.fft.fftfreq(n,L/n)
 		psi_f = vort_f/kappa
+
 		#psi is the term which determines incompressibility
-	
 		psi = np.fft.ifft(psi_f)
 
 		#compute new_vorticity using psi using a simple Euler time shift
-		vorticity = euler_shift(vorticity,psi,velocities,dt)
+		vorticity = euler_shift(vorticity,psi,velocities,dt,dx,diffusivity)
 	return vorticity
